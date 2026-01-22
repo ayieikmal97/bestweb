9ed7c6ba1f9bd1ccd3ff71ace35db5c74a0d66a8
Ikmal Bahari Bin Abd Halim <ikmalbahari.halim@gmail.com>
2026-01-22 09:35:05 +0800

    remove auth

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,12 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
-    Route::prefix('product')->group(function () {
-        Route::get('/', [ProductController::class, 'index']);
-        Route::get('/create', [ProductController::class, 'create']);           
-        Route::get('{product}', [ProductController::class, 'show']);    
-        Route::post('/', [ProductController::class, 'store']);           
-        Route::delete('{product}', [ProductController::class, 'destroy']); 
-        Route::delete('/', [ProductController::class, 'bulkDelete']);   
-    });
+
+Route::prefix('product')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/create', [ProductController::class, 'create']);           
+    Route::get('{product}', [ProductController::class, 'show']);    
+    Route::post('/', [ProductController::class, 'store']);           
+    Route::delete('{product}', [ProductController::class, 'destroy']); 
+    Route::delete('/', [ProductController::class, 'bulkDelete']);   
 });
